182363a11e3c74817c105a677bfb1d06f5f6028e
izz520 <15474741276@qq.com>
2024-08-14 15:58:13 +0800

    save

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,19 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import "forge-std/Script.sol"; // 引入foundry forge中的Script库
+import {Counter} from "../src/Counter.sol"; // 引入要部署的Counter合约
 
+// 部署脚本继承了Script合约
 contract CounterScript is Script {
     Counter public counter;
 
+    // 可选函数，在每个函数运行之前被调用
     function setUp() public {}
 
+    // 部署合约时会调用run()函数
     function run() public {
+        // 开始记录脚本中合约的调用和创建
         vm.startBroadcast();
-
+        // 创建合约
         counter = new Counter();
-
+        // 结束记录
         vm.stopBroadcast();
     }
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,23 +1,36 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console, console2} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
 
+    // 初始化测试用例
     function setUp() public {
         counter = new Counter();
         counter.setNumber(0);
     }
 
+    // 基于初始化测试用例
+    // 断言测试自增后的 counter 的 number 返回值 同等于 1
     function test_Increment() public {
         counter.increment();
         assertEq(counter.number(), 1);
     }
 
+    // 基于初始化测试用例
+    // 执行差异测试测试
+    // forge 测试的过程中
+    // 为 testSetNumber 函数参数传递不同的 unit256 类型的 x
+    // 达到测试 counter 的 setNumber 函数 为不同的 x 设置不同的数
+    // 断言 number() 的返回值等同于差异测试的 x 参数
     function testFuzz_SetNumber(uint256 x) public {
+        console2.log("testNumber1: %s==============", x);
+        console2.log("testNumber2: %s==============", x);
+        emit log("test");
+        console2.log("before:", block.timestamp);
         counter.setNumber(x);
         assertEq(counter.number(), x);
     }
